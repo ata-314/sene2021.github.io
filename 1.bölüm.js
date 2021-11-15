d904af9a913069b3a850abee8bba081151797b88
ata-314 <83098505+ata-314@users.noreply.github.com>
2021-11-15 12:56:38 +0300

    Sene 2021

---
+++ 1.bölüm.js
@@ -0,0 +1,10 @@
+const link = document.querySelector('.box')
+link.style.display="none"
+
+function bigImg() {
+    link.style.display=""
+  }
+  function bigImg2() {
+    link.style.display="none"
+  }
+
